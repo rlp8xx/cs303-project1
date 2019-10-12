29f54d6d87180ec6fb6f9d4e8d4ca78f9b6c9cf3
Bobby Plubell <rlp8xx@mail.umkc.edu>
2019-10-12 14:25:17 -0500

    Re-implementing correct type of queue...

--- Queue.hpp
+++ Queue.hpp
@@ -1,77 +1,28 @@
 #include <iostream>
+#include <string>
+
 #define NDEBUG
 
 using namespace std;
 
-template <class T>
-class QueueNode;
+class PriorityQueueNode;
 
-template <class T>
-class MyQueue {
-	public:
-		void add_item(T* data) {
-			// empty queue
-			if (tail == NULL && head == NULL) {
-        #ifdef NDEBUG
-          cout << "Adding new node to empty queue" << endl;
-        #endif
-				tail = head = new QueueNode<T>(data);
-			  return;
-			}
-      #ifdef NDEBUG
-        cout << "Adding new node to queue" << endl;
-      #endif
-			// set the next pointer on the tail
-			this->tail->next = new QueueNode<T>(data);
-			// set the tail to what's current the next pointer on current tail
-			this->tail = this->tail->next;
-		}
-
-		T* pop() {
-			if (tail == NULL && head == NULL) {
-        #ifdef NDEBUG
-          cout << "Tried to pop empty queue, returning null" << endl;
-        #endif
-        return NULL;
-      }
-			if (tail == head) {
-        #ifdef NDEBUG
-          cout << "Popping while Tail = Head" << endl;
-        #endif
-				T* val = head->data;
-        delete head;
-				tail = head = NULL;
-        #ifdef NDEBUG
-          cout << "Tail and head now nulled by pop with tail=head" << endl;
-        #endif
-				return val;
-			}
-      #ifdef NDEBUG
-        cout << "Popping" << endl;
-      #endif
-			T* val = this->head->data;
-      QueueNode<T>* old_head = head;
-      this->head = this->head->next;
-      delete old_head;
-      return val;
-		}
-
-		T* first() {
-			return this->head->data;
-		}
-
+class PriorityQueue {
+  public:
+    void push(string data, int priority) {
+      
+    }
   private:
-		QueueNode<T>* head = NULL;
-		QueueNode<T>* tail = NULL;
+    PriorityQueueNode* items;
+    
 };
 
-template <class T>
-class QueueNode {
-	public:
-		QueueNode(T* data) {
-			this->data = data;
-		}
-    
-		T* data;
-		QueueNode<T>* next = NULL;
-};
+class PriorityQueueNode {
+  public:
+    int priority;
+    string data;
+  
+    bool operator<(PriorityQueueNode const &a, PriorityQueueNode const &b) {
+      return a->data < b->data;
+    };
+}
