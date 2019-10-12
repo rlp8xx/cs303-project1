fec58824fcc58d438d3266c9571c9b7b8979b2d3
Bobby Plubell <rlp8xx@mail.umkc.edu>
2019-10-12 13:49:16 -0500

    Queue and tests working

--- Queue.hpp
+++ Queue.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define NDEBUG
 
 using namespace std;
 
@@ -8,34 +9,54 @@ class QueueNode;
 template <class T>
 class MyQueue {
 	public:
-		void add_item(T data) {
+		void add_item(T* data) {
 			// empty queue
 			if (tail == NULL && head == NULL) {
+        #ifdef NDEBUG
+          cout << "Adding new node to empty queue" << endl;
+        #endif
 				tail = head = new QueueNode<T>(data);
 			  return;
 			}
+      #ifdef NDEBUG
+        cout << "Adding new node to queue" << endl;
+      #endif
 			// set the next pointer on the tail
 			this->tail->next = new QueueNode<T>(data);
 			// set the tail to what's current the next pointer on current tail
 			this->tail = this->tail->next;
 		}
 
-		T pop() {
-			if (tail == NULL && head == NULL) return NULL;
+		T* pop() {
+			if (tail == NULL && head == NULL) {
+        #ifdef NDEBUG
+          cout << "Tried to pop empty queue, returning null" << endl;
+        #endif
+        return NULL;
+      }
 			if (tail == head) {
-				T val = head->data;
+        #ifdef NDEBUG
+          cout << "Popping while Tail = Head" << endl;
+        #endif
+				T* val = head->data;
         delete head;
 				tail = head = NULL;
+        #ifdef NDEBUG
+          cout << "Tail and head now nulled by pop with tail=head" << endl;
+        #endif
 				return val;
 			}
-			T val = this->head->data;
+      #ifdef NDEBUG
+        cout << "Popping" << endl;
+      #endif
+			T* val = this->head->data;
       QueueNode<T>* old_head = head;
       this->head = this->head->next;
       delete old_head;
       return val;
 		}
 
-		T first() {
+		T* first() {
 			return this->head->data;
 		}
 
@@ -47,10 +68,10 @@ class MyQueue {
 template <class T>
 class QueueNode {
 	public:
-		QueueNode(T data) {
+		QueueNode(T* data) {
 			this->data = data;
 		}
-
-		T data;
+    
+		T* data;
 		QueueNode<T>* next = NULL;
 };
--- Queue_Test.cpp
+++ Queue_Test.cpp
@@ -13,23 +13,23 @@ void test1() {
   MyQueue<string>* q = new MyQueue<string>();
 	string test = "test string";
 	cout << "Adding " << test << endl;
-	q->add_item(test);
+	q->add_item(&test);
 	string test2 = "test string 2";
 	cout << "Adding " << test2 << endl;
-	q->add_item(test2);
+	q->add_item(&test2);
   string test3 = "test string 3";
   cout << "Adding " << test3 << endl;
-  q->add_item(test3);
+  q->add_item(&test3);
 
-  string first = q->pop();
-  string second = q->pop();
-  string third = q->pop();
-	cout << "Top item: " << first << endl;
-  assert(first == test);
-	cout << "Top-1 item: " << second << endl;
-  assert(second == test2);
-  cout << "Top-2 item: " << third << endl;
-  assert(third == test3);
+  string* first = q->pop();
+  string* second = q->pop();
+  string* third = q->pop();
+	cout << "Top item: " << *first << endl;
+  assert(*first == test);
+	cout << "Top-1 item: " << *second << endl;
+  assert(*second == test2);
+  cout << "Top-2 item: " << *third << endl;
+  assert(*third == test3);
   cout << "Test 1 complete." << endl << endl;
 }
 
@@ -37,13 +37,16 @@ void test2() {
   cout << "Test 2" << endl;
   MyQueue<string>* q = new MyQueue<string>();
   string test = "asdf 1234";
-  q->add_item(test);
+  q->add_item(&test);
   q->pop();
-  string should_be_null = q->pop();
-  assert(should_be_null == NULL);
-  q->add_item(test);
-  string should_be_test = q->pop();
-  assert(should_be_test == test);
+  cout << "Pushed and then popped test string" << endl;
+  q->pop();
+  cout << "Popped on empty queue" << endl;
+  q->add_item(&test);
+  cout << "Added test string again" << endl;
+  string* should_be_test = q->pop();
+  cout << "Popped test string" << endl;
+  assert(*should_be_test == test);
   cout << "Test 2 complete." << endl << endl;
 }
 
